f3f80e10dd5c77468a4689de4f0b60d251647d7f
ef1menko <efimenko.michael@gmail.com>
2022-12-18 17:10:40 +0300

    22

--- 1progg.py
+++ 1progg.py
@@ -5,4 +5,4 @@ for calls in range (0, debtors, 5):
     print('Должник номер:', calls)
     credit += int(input('Введите сумму долга: '))
     print()
-print('Общая сумма долга: ', credit)
+print('Общая суfdfdмма долга: ', credit)
