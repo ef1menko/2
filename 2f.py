a5c620ad2c8710a6058677ad97f11d8de2655eb9
ef1menko <efimenko.michael@gmail.com>
2022-12-18 19:19:26 +0300

    123123123

--- 2f.py
+++ 2f.py
@@ -3,6 +3,6 @@ print()
 credit = 0
 for calls in range (0, debtors, 5):
     print('Должник номvер:', calls)
-    credit += int(input('Введите сумму долга: '))
+    credit += int(input('Введиffffте сумму долга: '))
     print()
 print('Общая сумма долга: ', credit)
